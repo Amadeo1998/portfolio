7d3255e094879bae69d7e06222b92d6452d589e1
Amadeo Noels <amadeo@origen.studio>
2023-03-09 22:49:43 +0100

    update page Uganda

--- pages/index.js
+++ pages/index.js
@@ -40,23 +40,36 @@ const App = () => (
             </div>
           </div>
           <div className='section' style={{zIndex: -1, position: "fixed"}}>
-            <Image src="/images/ayinet/vlagUganda.jpeg" layout='fill' objectFit="cover" alt='group picture' className='darkened-image'/>
+            <Image src="/images/ayinet/backgroundUganda.jpeg" layout='fill' objectFit="cover" alt='group picture' className='darkened-image'/>
           </div>
-          <div className="h-screen flex justify-center">
-          <div className='grid grid-cols-4 gap-4 mt-10'>
-              <Image className='rounded-lg' width={400} height={100} src='/images/minor/atonium.jpg' alt='test'/>
-              <Image className='rounded-lg' width={400} height={250} src='/images/minor/koninklijkPaleis.jpg' alt='test'/>
-              <Image className='rounded-lg' width={400} height={250} src='/images/minor/kruger.jpg' alt='test'/>
-              <Image className='rounded-lg' width={400} height={250} src='/images/minor/waterpretpark.jpg' alt='test'/>
-          </div>
-          </div>
+          <div className='h-screen grid content-center grid-rows-3'>
+            <div className='grid grid-rows-1 grid-flow-col justify-items-center mt-10'>
+                <Image className='rounded-lg' width={400} height={300} src='/images/ayinet/ontbijt.jpg' alt='test'/>
+                <Image className='rounded-lg' width={400} height={600} src='/images/ayinet/ayinetFolder.jpg' alt='test'/>
+                <Image className='rounded-lg' width={400} height={600} src='/images/ayinet/office.jpg' alt='test'/>
+              
+            </div>
+            <div className='text-2xl text-white text-image justify-items-center'>
+              <p className='flow-root p-24 text-4xl leading-loose'>A ... in Uganda offered me a great opportunity to improve and develop my skills such as: communication, teamwork, leadership, problem-solving thinking.
+
+Furthermore, this internship offered me an enrichment of a new culture. Thus, I experienced new traditions and a different way of life.</p>
+            </div>
+
+            <div className='grid grid-rows-1 grid-flow-col justify-items-center mb-10'>
+              
+              <Image className='rounded-lg' width={400} height={600} src='/images/ayinet/ayinet.jpg' alt='test'/>
+              
+              <Image className='rounded-lg' width={400} height={600} src='/images/ayinet/huis.jpg' alt='test'/>
+              <Image className='rounded-lg' width={400} height={600} src='/images/ayinet/aap.jpg' alt='test'/>
+            </div>
+         </div>
         
           <div className='section' style={{zIndex: -1, position: "fixed"}}>
             <Image src="/images/minor/group.jpg" layout='fill' objectFit="cover" alt='group picture' className='darkened-image'/>
           </div>
-          <div className="ml-10 grid grid-rows-3 h-100vh">
-            <div className='grid grid-cols-4 gap-4 mt-10'>
-              <Image className='rounded-lg' width={400} height={100} src='/images/minor/atonium.jpg' alt='test'/>
+          <div className="ml-10 grid grid-rows-3">
+            <div className='grid grid-cols-4 gap-4 mb-10'>
+              <Image className='rounded-lg ' width={400} height={100} src='/images/minor/atonium.jpg' alt='test'/>
               <Image className='rounded-lg' width={400} height={250} src='/images/minor/koninklijkPaleis.jpg' alt='test'/>
               <Image className='rounded-lg' width={400} height={250} src='/images/minor/kruger.jpg' alt='test'/>
               <Image className='rounded-lg' width={400} height={250} src='/images/minor/waterpretpark.jpg' alt='test'/>
@@ -79,7 +92,7 @@ const App = () => (
             </div>
             
           </div>
-          <div className='grid grid-cols-4 gap-4 mt-14 flex justify-between'>
+          <div className='grid grid-cols-4 gap-4 mt-14'>
             <Image className='rounded-lg' width={400} height={100} src='/images/minor/boat.jpg' alt='test'/>
             <Image className='rounded-lg' width={400} height={250} src='/images/minor/sunsetBoat.jpg' alt='test'/>
           <div>
